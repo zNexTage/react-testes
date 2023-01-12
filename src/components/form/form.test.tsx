c3d811eb1cd2a46c283532ef87042630af9967d9
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-01-12 08:23:52 -0300

    add new participant in list;
    after type the participant name and submit the form, the participant will be added to the list.

--- src/components/form/form.test.tsx
+++ src/components/form/form.test.tsx
@@ -1,11 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 import Form from './form';
 
 //Jest
-test('when the input is empty, new participants cannot be added', ()=>{
-    render(<Form />);
-    
+test('when the input is empty, new participants cannot be added', () => {
+    render(
+        <RecoilRoot>
+            <Form />
+        </RecoilRoot>
+    );
+
     //Find in DOM the input
     const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
 
@@ -17,4 +22,30 @@ test('when the input is empty, new participants cannot be added', ()=>{
 
     //checks if button is disabled.
     expect(button).toBeDisabled();
+});
+
+test('add a new participants when the participant name field is not blank', () => {
+    render(
+        <RecoilRoot>
+            <Form />
+        </RecoilRoot>
+    );
+
+    //Find in DOM the input
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+
+    //Find the button
+    const button = screen.getByRole('button');
+
+    // Input value
+    fireEvent.change(input, { target: { value: 'Fernanda Takai' } });
+
+    // Submit form
+    fireEvent.click(button);
+
+    // Clean input
+    expect(input).toHaveValue('');
+
+    // Focus in input
+    expect(input).toHaveFocus();
 });
--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,10 +1,37 @@
-const Form = ()=>{
+import React, { useRef, useState } from "react";
+import useAddParticipant from "../../state/hooks/useAddParticipant";
+
+const Form = () => {
+    const [name, setName] = useState<string>('');
+
+    const participantInput = useRef<HTMLInputElement>(null);
+
+    const addParticipant = useAddParticipant();
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => setName(event.target.value);
+
+    const onSubmit = (event:React.FormEvent<HTMLFormElement>) =>{
+        event.preventDefault();
+
+        setName('');
+
+        participantInput.current?.focus();
+
+        addParticipant(name);
+    }
+
     return (
-        <form>
-            <input 
-            type="text" 
-            placeholder="Insira os nomes dos participantes" />
-            <button disabled>
+        <form onSubmit={onSubmit}>
+            <input
+                ref={participantInput}
+                value={name}
+                onChange={onChange}
+                type="text"
+                placeholder="Insira os nomes dos participantes" />
+
+            <button
+                disabled={!name}
+            >
                 Adicionar
             </button>
         </form>
